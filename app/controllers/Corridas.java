2f054e997f8e8414bde8b23f5a69c7c2e793b151
Natan Paulo <natanpaulo1300@gmail.com>
2025-05-29 16:48:39 -0300

    pesquisando com local

--- app/controllers/Corridas.java
+++ app/controllers/Corridas.java
@@ -5,37 +5,46 @@ import java.util.List;
 import models.Corrida;
 import play.mvc.Controller;
 
-public class Corridas  extends Controller{
+public class Corridas extends Controller {
 	public static void form() {
 		render();
 	}
-	
+
 	public static void principal() {
-		List<Corrida> corridas = Corrida.findAll();
+		// recebe o valor que esta no input de pesquisa no html
+		String busca = params.get("busca");
+		List<Corrida> corridas = null;
 		float meutotal = 0;
-	
-		for(Corrida cc: corridas) {
+		
+
+		if (busca == null) {
+			corridas = Corrida.findAll();
+		} else {
+			corridas = Corrida.find("byLocal", busca).fetch();
+		}
+
+		for (Corrida cc : corridas) {
 
 			meutotal += cc.kmpercorridos;
 		}
-		
-		render(meutotal,corridas);
+		render(meutotal, corridas);
 	}
-	
+
 	public static void salvar(Corrida c) {
-		if(c != null) {
+		if (c != null) {
 			c.save();
 		}
 		principal();
 	}
-	
+
 	public static void excluir(long id) {
 		Corrida c = Corrida.findById(id);
 		c.delete();
 		principal();
 	}
+
 	public static void editar(long id) {
 		Corrida c = Corrida.findById(id);
-		renderTemplate("Corridas/form.html", c); 
+		renderTemplate("Corridas/form.html", c);
 	}
 }
